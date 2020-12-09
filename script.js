fe7b7c275afcb0550cb2d52967996130665abaa3
Norbert Oláh <olahnorbi02@gmail.com>
2020-12-09 18:43:26 +0100

    login js

---
+++ script.js
@@ -0,0 +1,65 @@
+var nameUser = "";
+var classroomUser = "";
+var emailUser = "";
+var omUser = "";
+var teacherUser = false;
+var emailData = "";
+var omData = "";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD9YNIhkl9msLIA8-Q0ND0k41CWY4gSo3w",
+    authDomain: "pontkoveto.firebaseapp.com",
+    projectId: "pontkoveto",
+    storageBucket: "pontkoveto.appspot.com",
+    messagingSenderId: "1095203857581",
+    appId: "1:1095203857581:web:f94ec69a0931212ba19224",
+    measurementId: "G-BGPH18HJK0"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function init() {
+    $("loginButton").addEventListener("onclick", login, false);
+    $("regButton").addEventListener("onclick", registration, false);
+    $("regPageButton").addEventListener("onclick", backToRegPage, false);
+    $("loginPageButton").addEventListener("onclick", backToLoginPage, false);
+}
+
+function login(emailUser, omUser, teacherUser, emailData, omData) {
+    var successful = false;
+    emailUser = $("inputEmailLogin").value;
+    omUser = $("inputOMLogin").value;
+    teacherUser = $("inputTeacherELogin").value;
+    if (!teacherUser) {
+        //logika
+        if (successful) {
+            location.href = "diaktablazat.html";
+        } else {
+            //hiba
+        }
+    } else {
+        //logika
+        if (successful) {
+            location.href = "diaktablazat.html";
+        } else {
+            //hiba
+        }
+    }
+}
+
+function registration(nameUser, emailUser, classroomUser, omUser, teacherUser) {
+
+}
+
+function backToRegPage() {
+    location.href = "regsztracio.html";
+}
+
+function backToLoginPage() {
+    location.href = "index.html";
+}
