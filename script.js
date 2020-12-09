899eddce6fe85870356a9ceec70501fcc5f3f2cc
Norbert Oláh <olahnorbi02@gmail.com>
2020-12-09 19:37:13 +0100

    asdas

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-var nameUser = "";
-var classUser = "";
-var emailUser = "";
-var omUser = "";
-var teacherUser = false;
-var emailData = "";
-var omData = "";
-
-var firebaseConfig = {
-    apiKey: "AIzaSyD9YNIhkl9msLIA8-Q0ND0k41CWY4gSo3w",
-    authDomain: "pontkoveto.firebaseapp.com",
-    projectId: "pontkoveto",
-    storageBucket: "pontkoveto.appspot.com",
-    messagingSenderId: "1095203857581",
-    appId: "1:1095203857581:web:f94ec69a0931212ba19224",
-    measurementId: "G-BGPH18HJK0"
-};
-
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-init();
-
 function $(id) {
     return document.getElementById(id);
 }
@@ -88,3 +65,27 @@ function backToRegPage() {
 function backToLoginPage() {
     location.replace = "https://jxn01.github.io/Pontkoveto-Rendszer/index.html";
 }
+
+var nameUser = "";
+var classUser = "";
+var emailUser = "";
+var omUser = "";
+var teacherUser = false;
+var emailData = "";
+var omData = "";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD9YNIhkl9msLIA8-Q0ND0k41CWY4gSo3w",
+    authDomain: "pontkoveto.firebaseapp.com",
+    projectId: "pontkoveto",
+    storageBucket: "pontkoveto.appspot.com",
+    messagingSenderId: "1095203857581",
+    appId: "1:1095203857581:web:f94ec69a0931212ba19224",
+    measurementId: "G-BGPH18HJK0"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+init();
+
